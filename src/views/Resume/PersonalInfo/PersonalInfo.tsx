de5d3940ad83342e8de241c69a196ee26e099470
eudemonia <173537077@qq.com>
2022-10-10 18:34:38 +0800

    ~

--- src/views/Resume/PersonalInfo/PersonalInfo.tsx
+++ src/views/Resume/PersonalInfo/PersonalInfo.tsx
@@ -4,7 +4,7 @@ import { ReactComponent as Scroll } from './icon/scroll.svg'
 import gsap from 'gsap'
 import { TextPlugin } from 'gsap/TextPlugin'
 import Skill from './Skill'
-import { GithubOutlined, ZhihuOutlined } from '@ant-design/icons'
+import { GithubOutlined, ZhihuOutlined, PhoneOutlined, MediumOutlined } from '@ant-design/icons'
 import { ReactComponent as TranfomerSvg } from './icon/tranfomer.svg'
 import './PersonalInfo.module.less'
 
@@ -12,7 +12,9 @@ const PersonalInfo = (): JSX.Element => {
   useEffect(() => {
     gsap.registerPlugin(TextPlugin)
     const tl = gsap.timeline();
-    tl.to('.logo_wrap', { text: 'The Martrix', duration: 1 });
+    tl.to('.logo_title', { text: 'The Martrix', duration: 1 });
+
+    tl.to('.TranfomerSvg path', { fill: 'red', duration: 2 });
 
     gsap.fromTo('.guide_svg path', { y: -200 }, { duration: 3, y: 0, fill: '#fff', repeat: Infinity })
   }, [])
@@ -21,7 +23,8 @@ const PersonalInfo = (): JSX.Element => {
 
       <div className='header_wrap'>
         <div className='logo_wrap'>
-          <TranfomerSvg />
+          <TranfomerSvg className='TranfomerSvg'/>
+          <p className='logo_title'></p>
         </div>
         <div className='my_social_contact'>
           <a href='https://github.com/IAMSBLOL/resume' target='_blank' rel="noreferrer"><GithubOutlined /></a>
@@ -31,8 +34,34 @@ const PersonalInfo = (): JSX.Element => {
       </div>
       <div className='content_wrap'>
         <div className='personal_info_wrap'>
-          <h3 className='ch_city'>深圳</h3>
-          <h2 className='en_city'>SEHN ZHEN</h2>
+          <div className='city_bg_wrap'>
+            <h3 className='ch_city'>深圳</h3>
+            <h2 className='en_city'>SEHN ZHEN</h2>
+          </div>
+
+          <div className='descriptions'>
+            <h1 className='title'>Online Resume</h1>
+
+            <p className='text'>
+              本人从事切图5年有余,在切图方面颇有心得...
+            </p>
+            <p className='text'>
+              算了,摊牌了.
+            </p>
+            <div>
+              <span className='tag nineninesix'>1222</span><span className='tag no_money'>123123</span>
+            </div>
+
+            <div className='contact_wrap'>
+              <div className='contact_item'>
+                <MediumOutlined /> 称呼: Matrix
+              </div>
+              <div className='contact_item'>
+                <PhoneOutlined /> 联系方式: 15219267088
+              </div>
+
+            </div>
+          </div>
         </div>
         <div className='three_skill_wrap'>
           <Skill />
